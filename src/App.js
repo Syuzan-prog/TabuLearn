96e01aa95fbe2c28ed565c482df3502af132fdc8
Syuzanna <syuzanna.ant@gmail.com>
2021-02-01 17:35:59 +0400

    'sginUp

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import PersonalData from "./components/personalData";
+import PersonalData from "./components/PersonalData";
 import CompanyDetails from "./components/CompanyDetails";
-import Password from "./components/password";
+import Password from "./components/Password";
+import SignIn from "./components/SignIn";
 
 class App extends React.Component {
   render() {
@@ -21,7 +22,10 @@ class App extends React.Component {
                   <Link to="/components/CompanyDetails">company</Link>
                 </li>
                 <li className="nav-link">
-                  <Link to="/components/password">password</Link>
+                  <Link to="/components/Password">password</Link>
+                </li>
+                <li className="nav-link">
+                  <Link to="/components/SignIn">SignIn</Link>
                 </li>
               </ul>
             </nav>
@@ -32,8 +36,11 @@ class App extends React.Component {
               <Route path="/components/CompanyDetails">
                 <CompanyDetails/>
               </Route>
-              <Route path="/components/password">
+              <Route path="/components/Password">
                 <Password />
+              </Route>
+              <Route path="/components/SignIn">
+                <SignIn />
               </Route>
             </Switch>
           </div>
--- src/components/Password/Password.jsx
+++ src/components/Password/Password.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Button, Form} from "react-bootstrap";
 
 import './Password.scss';
+import logo from '../../assets/icons/clarity_eye-hide-line.svg'
 
 export const Password = (props) => {
     return (
@@ -21,8 +22,9 @@ export const Password = (props) => {
                             name="password"
                             id="password"
                             className="passwordInput"
-                            placeholder="Pasword(min. 8 characters)"
+                            placeholder="Password(min. 8 characters)"
                         />
+                        <img src={logo} alt="" className="passwordIcon"/>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label htmlFor="confirmPassword" className="passwordLabel">
@@ -35,10 +37,15 @@ export const Password = (props) => {
                             className="passwordInput"
                             placeholder="*****"
                         />
+                        <img src={logo} alt="" className="passwordIcon"/>
                     </Form.Group>
                     <Button disabled className="passwordButton">
                         SIgn Up
                     </Button>
+                    <Form.Text className="text-muted passwordFormText ">
+                        By signing up, I agree to TabuLearn’s <br/>
+                        <a href="/">Terms and Conditions.</a>
+                    </Form.Text>
                 </Form>
             </div>
         </div>
